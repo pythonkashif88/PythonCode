52f522aa615e0bf2ce64f041cf1b3ad7c3b1527b
pythonkashif88 <python.kashif88@gmail.com>
2025-08-03 13:10:49 +0530

    PythonAssignment1
    PythonAssignment1

---
+++ PythonAssignment1.py
@@ -0,0 +1,15 @@
+def mathsOperations():
+    vNum1 = int(input("Enter the first Number: "))
+    vNum2 = int(input("Enter the second Number: "))
+    print("The results are: \n")
+    print("Addition ", vNum1 + vNum2)
+    print("Subtraction ", vNum1 - vNum2)
+    print("Multiplication ", vNum1 * vNum2)
+    print("Division ", vNum1 / vNum2)
+	
+def combineNames():
+    vFirstName = input("Enter the first name :")
+    vLastName = input("Enter the second name :")
+    vFullName = vFirstName +' ' + vLastName
+    print("Hello "+ vFullName +" welcome to Python code")
+		
